cc42d4c4961039567571f4b5a2dec0403b8ff646
emreturgutce <emrebursalee@gmail.com>
2020-10-26 18:39:13 +0300

    type-graphql added to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,23 +5,29 @@ import {
   Entity,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { Field, ID, ObjectType } from 'type-graphql';
 
+@ObjectType()
 @Entity()
 export class User extends BaseEntity {
+  @Field(() => ID)
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
+  @Field()
   @Column({ unique: true })
   email!: string;
 
   @Column()
   password!: string;
 
+  @Field()
   @CreateDateColumn({
     type: 'timestamp',
   })
   createdAt!: Date;
 
+  @Field()
   @CreateDateColumn({
     type: 'timestamp',
   })
