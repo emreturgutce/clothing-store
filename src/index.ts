af1b4e10fac4552ff352c7161f17383b45a0991a
emreturgutce <emrebursalee@gmail.com>
2020-10-26 18:38:59 +0300

    main function created and schema builder added

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,37 @@
 import 'reflect-metadata';
 import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import { createConnection } from 'typeorm';
+import { buildSchema } from 'type-graphql';
+import path from 'path';
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+async function main() {
+  createConnection()
+    .then(() => {
+      console.log(`🐘 Connected to Postgres DB`);
+    })
+    .catch((err) => {
+      console.error(`❌ Error occurred connecting Postgres DB:\n${err}`);
+      process.exit(1);
+    });
 
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-};
-
-createConnection()
-  .then(() => {
-    console.log(`🐘 Connected to Postgres DB`);
-  })
-  .catch((err) => {
-    console.error(`❌ Error occurred connecting Postgres DB:\n${err}`);
-    process.exit(1);
+  const schema = await buildSchema({
+    resolvers: [path.join(__dirname, '/resolvers/**/*.ts')],
   });
 
-const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({ schema });
 
-const app = express();
+  const app = express();
 
-server.applyMiddleware({ app });
+  server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () => {
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  app.listen({ port: 4000 }, () => {
+    console.log(
+      `🚀 Server ready at http://localhost:4000${server.graphqlPath}`,
+    );
+  });
+}
+
+main().catch((err) => {
+  console.error(err);
 });
