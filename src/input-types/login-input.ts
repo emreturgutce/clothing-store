647f349a59c3290be49a7b56d0b1caee6e5e92bd
emreturgutce <emrebursalee@gmail.com>
2020-10-27 13:30:43 +0300

    login input added

---
+++ src/input-types/login-input.ts
@@ -0,0 +1,13 @@
+import { IsEmail, Length } from 'class-validator';
+import { Field, InputType } from 'type-graphql';
+
+@InputType()
+export class LoginInput {
+  @Field()
+  @IsEmail()
+  email!: string;
+
+  @Field()
+  @Length(6, 255)
+  password!: string;
+}
