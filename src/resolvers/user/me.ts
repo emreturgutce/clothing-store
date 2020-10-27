e986620f5a1abab65a29caf0c39cc97eb2dad44a
emreturgutce <emrebursalee@gmail.com>
2020-10-27 12:32:46 +0300

    authorized me route,

---
+++ src/resolvers/user/me.ts
@@ -0,0 +1,26 @@
+import { Authorized, Ctx, Mutation, Resolver } from 'type-graphql';
+import jwt from 'jsonwebtoken';
+import { User } from '../../models/user';
+import { Context } from '../../types/context';
+import { JWT_SECRET } from '../../config';
+
+@Resolver()
+export class LoginUserResolver {
+  @Authorized()
+  @Mutation(() => User, { nullable: true })
+  async me(@Ctx() { req }: Context): Promise<User | null> {
+    const id = jwt.verify(req.session!.userId, JWT_SECRET);
+
+    if (!id) {
+      return null;
+    }
+
+    const user = await User.findOne({ where: { id } });
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+}
