487bea315a867bf4007ec9ef343ab3c879d990fe
emreturgutce <emrebursalee@gmail.com>
2020-10-28 10:15:39 +0300

    logout query

---
+++ src/resolvers/user/logout.ts
@@ -0,0 +1,23 @@
+import { Authorized, Ctx, Query, Resolver } from 'type-graphql';
+import { COOKIE_NAME } from '../../constants';
+import { Context } from '../../types/context';
+
+@Resolver()
+export class MeResolver {
+  @Authorized()
+  @Query(() => Boolean)
+  async logout(@Ctx() ctx: Context): Promise<Boolean | any> {
+    return new Promise((resolve, reject) => {
+      ctx.req.session!.destroy((err) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        }
+
+        ctx.res.clearCookie(COOKIE_NAME);
+
+        return resolve(true);
+      });
+    });
+  }
+}
