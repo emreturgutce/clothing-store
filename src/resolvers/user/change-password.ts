9c344eb321b6125d9a2facad727a2cec4a57e7ff
emreturgutce <emrebursalee@gmail.com>
2020-10-28 11:35:26 +0300

    change password mutation added

---
+++ src/resolvers/user/change-password.ts
@@ -0,0 +1,38 @@
+import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
+import bcrypt from 'bcryptjs';
+import { FORGOT_PASSWORD_PREFIX } from '../../constants';
+import { User } from '../../models/user';
+import { redis } from '../../config/redis';
+import { ChangePasswordInput } from '../../input-types/change-password-input';
+import { Context } from '../../types/context';
+
+@Resolver()
+export class ChangePasswordResolver {
+  @Mutation(() => User, { nullable: true })
+  async changePassword(
+    @Arg('data') { token, password }: ChangePasswordInput,
+    @Ctx() ctx: Context,
+  ): Promise<User | null> {
+    const userId = await redis.get(`${FORGOT_PASSWORD_PREFIX}${token}`);
+
+    if (!userId) {
+      return null;
+    }
+
+    const user = await User.findOne(userId);
+
+    if (!user) {
+      return null;
+    }
+
+    await redis.del(`${FORGOT_PASSWORD_PREFIX}${token}`);
+
+    user.password = await bcrypt.hash(password, 12);
+
+    await user.save();
+
+    ctx.req.session!.userId = user.id;
+
+    return user;
+  }
+}
