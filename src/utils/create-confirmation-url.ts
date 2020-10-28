98dc83fa33b1161790370cb178c9a232bd8148db
emreturgutce <emrebursalee@gmail.com>
2020-10-28 11:34:27 +0300

    creating unique url for email confirmation

---
+++ src/utils/create-confirmation-url.ts
@@ -0,0 +1,13 @@
+import { v4 as uuid } from 'uuid';
+import { CONFIRM_USER_PREFIX } from '../constants';
+import { redis } from '../config/redis';
+
+export const createConfirmationUrl = async (
+  userId: string,
+): Promise<string> => {
+  const token = uuid();
+
+  await redis.set(`${CONFIRM_USER_PREFIX}${token}`, userId, 'ex', 60 * 60 * 24);
+
+  return `http://localhost:3000/user/confirm/${token}`;
+};
