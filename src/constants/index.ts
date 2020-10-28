28313669d6bec30486994820f3cfe740416a82d5
emreturgutce <emrebursalee@gmail.com>
2020-10-28 11:33:01 +0300

    constants for email confirmation and forgot password processes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,4 @@
 export const COOKIE_EXPIRATION = 1000 * 60 * 60 * 24 * 7 * 365;
 export const COOKIE_NAME = 'auth_token';
+export const CONFIRM_USER_PREFIX = 'user-confirmation:';
+export const FORGOT_PASSWORD_PREFIX = 'forgot-password:';
