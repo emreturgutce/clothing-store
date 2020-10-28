7afbc72c2c7b2359f08be429eb8fac88997977cc
emreturgutce <emrebursalee@gmail.com>
2020-10-28 10:53:04 +0300

    category model added

---
+++ src/models/category.ts
@@ -0,0 +1,32 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Field, ID, ObjectType } from 'type-graphql';
+
+@ObjectType()
+@Entity()
+export class Category extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Field()
+  @Column({ unique: true })
+  name!: string;
+
+  @Field({ nullable: true })
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  createdAt!: Date;
+
+  @Field({ nullable: true })
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  updatedAt!: Date;
+}
