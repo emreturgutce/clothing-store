5d632323915994f569ae3531e43bd1ec29cc6f88
emreturgutce <emrebursalee@gmail.com>
2020-10-28 11:35:14 +0300

    forgot password mutation resolver

---
+++ src/resolvers/user/forgot-password.ts
@@ -0,0 +1,34 @@
+import { Arg, Mutation, Resolver } from 'type-graphql';
+import { v4 as uuid } from 'uuid';
+import { FORGOT_PASSWORD_PREFIX } from '../../constants';
+import { User } from '../../models/user';
+import { redis } from '../../config/redis';
+import { sendEmail } from '../../utils/send-email';
+
+@Resolver()
+export class ForgotPasswordResolver {
+  @Mutation(() => Boolean)
+  async forgotPassword(@Arg('email') email: string): Promise<boolean> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return false;
+    }
+
+    const token = uuid();
+
+    await redis.set(
+      `${FORGOT_PASSWORD_PREFIX}${token}`,
+      user.id,
+      'ex',
+      60 * 60 * 24,
+    );
+
+    await sendEmail(
+      email,
+      `http://localhost:3000/user/change-password/${token}`,
+    );
+
+    return true;
+  }
+}
