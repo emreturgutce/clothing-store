2e3a7173087d34aad8e8382d92e16dec1a2eaab1
emreturgutce <emrebursalee@gmail.com>
2020-10-29 12:38:08 +0300

    creating product with owner and categories if provided

--- src/resolvers/product/add.ts
+++ src/resolvers/product/add.ts
@@ -1,18 +1,44 @@
-import { Arg, Mutation, Resolver } from 'type-graphql';
+import { Arg, Authorized, Ctx, Mutation, Resolver } from 'type-graphql';
+import jwt from 'jsonwebtoken';
 import { ProductInput } from '../../input-types/product-input';
+import { Category } from '../../models/category';
 import { Product } from '../../models/product';
+import { User } from '../../models/user';
+import { Context } from '../../types/context';
+import { JWT_SECRET } from '../../config';
 
 @Resolver()
 export class AddProductResolver {
-  @Mutation(() => Product)
+  @Authorized()
+  @Mutation(() => Product, { nullable: true })
   async add(
-    @Arg('data') { name, price, description, stock }: ProductInput,
-  ): Promise<Product> {
+    @Arg('data')
+    { name, price, description, stock, categoriesIds }: ProductInput,
+    @Ctx() { req }: Context,
+  ): Promise<Product | null> {
+    const userId = jwt.verify(req.session!.userId, JWT_SECRET);
+    const owner = await User.findOne({
+      where: { id: userId },
+      relations: ['detail'],
+    });
+
+    if (!owner) {
+      return null;
+    }
+
+    let categories: Category[] | undefined;
+
+    if (categoriesIds) {
+      categories = await Category.findByIds(categoriesIds);
+    }
+
     const product = Product.create({
       name,
       price,
       description,
       stock,
+      owner,
+      categories,
     }).save();
 
     return product;
