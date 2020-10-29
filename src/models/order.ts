44518db30b12fbdbdafb4e927ad19e9a621754a7
emreturgutce <emrebursalee@gmail.com>
2020-10-29 12:55:51 +0300

    order model implemented

---
+++ src/models/order.ts
@@ -0,0 +1,54 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Field, ID, ObjectType } from 'type-graphql';
+import { User } from './user';
+import { OrderProduct } from './order-product';
+
+enum Status {
+  complete = 'COMPLETE',
+  cancelled = 'CANCELLED',
+  pending = 'PENDING',
+}
+
+@ObjectType()
+@Entity()
+export class Order extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Field()
+  @OneToOne(() => User)
+  user!: User;
+
+  @Field()
+  @Column({
+    type: 'enum',
+    enum: Status,
+    default: Status.pending,
+  })
+  status!: Status;
+
+  @Field()
+  @OneToMany(() => OrderProduct, (orderProduct) => orderProduct.order)
+  orderProducts!: OrderProduct[];
+
+  @Field({ nullable: true })
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  createdAt!: Date;
+
+  @Field({ nullable: true })
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  updatedAt!: Date;
+}
