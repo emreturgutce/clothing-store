9bdaa3b52a3033f02ae9f89930321dd248dfdc1f
emreturgutce <emrebursalee@gmail.com>
2020-10-30 18:50:22 +0300

    change order status resolver

---
+++ src/resolvers/order/change-status.ts
@@ -0,0 +1,34 @@
+import { Arg, Authorized, Ctx, Query, Resolver } from 'type-graphql';
+import jwt from 'jsonwebtoken';
+import { Order, OrderStatus } from '../../models/order';
+import { Context } from '../../types/context';
+import { JWT_SECRET } from '../../config';
+
+@Resolver()
+export class ChangeOrderStatusResolver {
+  @Authorized()
+  @Query(() => Boolean, { nullable: true })
+  async changeOrderStatus(
+    @Arg('id') id: string,
+    @Arg('status') status: OrderStatus,
+    @Ctx() { req }: Context,
+  ): Promise<boolean> {
+    const userId = jwt.verify(req.session!.userId, JWT_SECRET);
+
+    const order = await Order.findOne(id, { relations: ['user'] });
+
+    if (!order) {
+      return false;
+    }
+
+    if (order.user.id !== userId) {
+      return false;
+    }
+
+    order.status = status;
+
+    await order.save();
+
+    return true;
+  }
+}
