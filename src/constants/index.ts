b84f8ddbaac467323450e5f6e771cfa438cacf47
emreturgutce <emrebursalee@gmail.com>
2020-10-30 19:09:00 +0300

    order expiration time const added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export const COOKIE_EXPIRATION = 1000 * 60 * 60 * 24 * 7 * 365;
 export const COOKIE_NAME = 'auth_token';
 export const CONFIRM_USER_PREFIX = 'user-confirmation:';
 export const FORGOT_PASSWORD_PREFIX = 'forgot-password:';
+export const ORDER_EXPIRATION_TIME = 60;
