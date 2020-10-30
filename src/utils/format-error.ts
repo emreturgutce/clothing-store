739af3c12f3d6a55e300d873b2685f4ab5c1c08e
emreturgutce <emrebursalee@gmail.com>
2020-10-30 17:03:23 +0300

    error handling utility function

---
+++ src/utils/format-error.ts
@@ -0,0 +1,24 @@
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
+
+export const formatError = (
+  err: GraphQLError,
+): GraphQLFormattedError<Record<string, any>> => {
+  if (err.message.match(/validation error/i)) {
+    const errors: string[] = [];
+
+    if (err.extensions) {
+      for (const { constraints } of err.extensions!.exception
+        .validationErrors) {
+        const error = Object.values(constraints).toString();
+
+        errors.push(error);
+      }
+    } else {
+      errors.push(err.message);
+    }
+
+    return { message: errors.toString() };
+  }
+
+  return err;
+};
