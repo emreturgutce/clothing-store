11a3fd9123cde177a252a1bfa2eb82122b8b5478
emreturgutce <emrebursalee@gmail.com>
2020-10-30 18:50:41 +0300

    get all orders resolver

---
+++ src/resolvers/order/get.ts
@@ -0,0 +1,21 @@
+import { Authorized, Query, Resolver } from 'type-graphql';
+import { Order } from '../../models/order';
+
+@Resolver()
+export class GetOrdersResolver {
+  @Authorized()
+  @Query(() => [Order], { nullable: true })
+  async getOrders(): Promise<Order[]> {
+    const orders = await Order.find({
+      join: {
+        alias: 'order',
+        leftJoinAndSelect: {
+          orderProducts: 'order.orderProducts',
+          product: 'orderProducts.product',
+        },
+      },
+    });
+
+    return orders;
+  }
+}
