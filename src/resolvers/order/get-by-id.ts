a84cebe9d28f3c69d569b6b6adec967e3355c003
emreturgutce <emrebursalee@gmail.com>
2020-10-30 18:50:52 +0300

    get order by id resolver

---
+++ src/resolvers/order/get-by-id.ts
@@ -0,0 +1,21 @@
+import { Arg, Authorized, Query, Resolver } from 'type-graphql';
+import { Order } from '../../models/order';
+
+@Resolver()
+export class GetOrderByIdResolver {
+  @Authorized()
+  @Query(() => Order, { nullable: true })
+  async getOrderById(@Arg('id') id: string): Promise<Order | undefined> {
+    const order = await Order.findOne(id, {
+      join: {
+        alias: 'order',
+        leftJoinAndSelect: {
+          orderProducts: 'order.orderProducts',
+          product: 'orderProducts.product',
+        },
+      },
+    });
+
+    return order;
+  }
+}
