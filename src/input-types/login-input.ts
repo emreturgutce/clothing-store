6bb477a4b02fe0d9e8d35853e652d059591f01db
emreturgutce <emrebursalee@gmail.com>
2020-10-30 17:05:54 +0300

    returning more meaningful messages after validation fails

--- src/input-types/login-input.ts
+++ src/input-types/login-input.ts
@@ -4,10 +4,10 @@ import { Field, InputType } from 'type-graphql';
 @InputType()
 export class LoginInput {
   @Field()
-  @IsEmail()
+  @IsEmail(undefined, { message: 'email must be valid' })
   email!: string;
 
   @Field()
-  @Length(6, 255)
+  @Length(6, 255, { message: 'password must be between 6 and 255 characters' })
   password!: string;
 }
