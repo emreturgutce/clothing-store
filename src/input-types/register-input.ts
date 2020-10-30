3066d4feca0b2f63ccdbf47b6416452f07e5cc02
emreturgutce <emrebursalee@gmail.com>
2020-10-30 17:06:02 +0300

    returning more meaningful messages after validation fails

--- src/input-types/register-input.ts
+++ src/input-types/register-input.ts
@@ -5,10 +5,12 @@ import { LoginInput } from './login-input';
 @InputType()
 export class RegisterInput extends LoginInput {
   @Field({ nullable: true })
-  @Length(2, 255)
+  @Length(2, 255, {
+    message: 'name must be between 2 and 255 characters',
+  })
   name?: string;
 
   @Field({ nullable: true })
-  @IsPhoneNumber('TR')
+  @IsPhoneNumber('TR', { message: 'phone number must be valid' })
   phone?: string;
 }
