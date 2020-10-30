2dfc3625caf45a00fb35c72599b075b8d9957c6d
emreturgutce <emrebursalee@gmail.com>
2020-10-30 13:17:55 +0300

    checking if the user actually exists with the given id

--- src/utils/auth-checker.ts
+++ src/utils/auth-checker.ts
@@ -1,16 +1,19 @@
 import jwt from 'jsonwebtoken';
 import { AuthChecker } from 'type-graphql';
 import { JWT_SECRET } from '../config';
+import { User } from '../models/user';
 import { Context } from '../types/context';
 
-export const authChecker: AuthChecker<Context> = ({ context }) => {
+export const authChecker: AuthChecker<Context> = async ({ context }) => {
   if (!context.req.session!.userId) {
     return false;
   }
 
-  const isValid = jwt.verify(context.req.session!.userId, JWT_SECRET);
+  const userId = jwt.verify(context.req.session!.userId, JWT_SECRET);
 
-  if (!isValid) {
+  const user = await User.findOne({ where: { id: userId } });
+
+  if (!user) {
     return false;
   }
 
