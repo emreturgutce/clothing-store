3093e7c306ed87592f634330b28227e25b94d80c
emreturgutce <emrebursalee@gmail.com>
2020-10-30 17:35:33 +0300

    get products by user

---
+++ src/resolvers/product/get-by-user.ts
@@ -0,0 +1,21 @@
+import { Authorized, Ctx, Query, Resolver } from 'type-graphql';
+import jwt from 'jsonwebtoken';
+import { Product } from '../../models/product';
+import { Context } from '../../types/context';
+import { JWT_SECRET } from '../../config';
+
+@Resolver()
+export class GetProductByUserResolver {
+  @Authorized()
+  @Query(() => [Product])
+  async getProductByUser(@Ctx() { req }: Context): Promise<Product[]> {
+    const id = jwt.verify(req.session!.userId, JWT_SECRET);
+
+    const product = await Product.find({
+      where: { owner: { id } },
+      relations: ['owner', 'categories'],
+    });
+
+    return product;
+  }
+}
