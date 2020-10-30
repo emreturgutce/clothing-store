c954a188be69cf4f6266ecbd25b50a228b137d44
emreturgutce <emrebursalee@gmail.com>
2020-10-30 18:51:07 +0300

    get orders by user resolver added

---
+++ src/resolvers/order/get-by-user.ts
@@ -0,0 +1,27 @@
+import { Authorized, Ctx, Query, Resolver } from 'type-graphql';
+import jwt from 'jsonwebtoken';
+import { Context } from '../../types/context';
+import { JWT_SECRET } from '../../config';
+import { Order } from '../../models/order';
+
+@Resolver()
+export class GetOrderByUserResolver {
+  @Authorized()
+  @Query(() => [Order])
+  async getOrderByUser(@Ctx() { req }: Context): Promise<Order[]> {
+    const id = jwt.verify(req.session!.userId, JWT_SECRET);
+
+    const order = await Order.find({
+      where: { user: { id } },
+      join: {
+        alias: 'order',
+        leftJoinAndSelect: {
+          orderProducts: 'order.orderProducts',
+          product: 'orderProducts.product',
+        },
+      },
+    });
+
+    return order;
+  }
+}
