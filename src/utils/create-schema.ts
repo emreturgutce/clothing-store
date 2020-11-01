5490a2b18bde58a7569b6f3eac098d1b0a257648
emreturgutce <emrebursalee@gmail.com>
2020-11-01 12:25:27 +0300

    using avatar scalar type

--- src/utils/create-schema.ts
+++ src/utils/create-schema.ts
@@ -1,10 +1,17 @@
 import { buildSchema } from 'type-graphql';
 import path from 'path';
 import { authChecker } from './auth-checker';
+import { AvatarScalar } from '../types/avatar-scalar';
 
 export const createSchema = async () => {
   return buildSchema({
     resolvers: [path.join(__dirname, '../resolvers/**/*.*')],
     authChecker,
+    scalarsMap: [
+      {
+        type: Buffer,
+        scalar: AvatarScalar,
+      },
+    ],
   });
 };
