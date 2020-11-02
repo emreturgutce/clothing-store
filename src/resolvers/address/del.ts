45bf3277884dadae8e7d974ac2f5fc84a1cb1b5d
emreturgutce <emrebursalee@gmail.com>
2020-11-02 12:41:22 +0300

    del address resolver added

---
+++ src/resolvers/address/del.ts
@@ -0,0 +1,39 @@
+import { Arg, Authorized, Ctx, Mutation, Resolver } from 'type-graphql';
+import jwt from 'jsonwebtoken';
+import { Address } from '../../models/address';
+import { Context } from '../../types';
+import { JWT_SECRET } from '../../config';
+import { User } from '../../models/user';
+
+@Resolver()
+export class DelAddressResolver {
+  @Authorized()
+  @Mutation(() => Boolean)
+  async delAddress(
+    @Arg('id') id: string,
+    @Ctx() { req }: Context,
+  ): Promise<boolean> {
+    if (!req.session) {
+      return false;
+    }
+
+    const userId = jwt.verify(req.session.userId, JWT_SECRET);
+
+    const user = await User.findOne({ where: { id: userId } });
+
+    if (!user) {
+      return false;
+    }
+
+    const result = await Address.delete({
+      id,
+      user,
+    });
+
+    if (!result.affected) {
+      return false;
+    }
+
+    return true;
+  }
+}
