583d33a4e1c3f6c8d1b7673856c64f8140093ad7
emreturgutce <emrebursalee@gmail.com>
2020-11-03 14:23:39 +0300

    comments

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
-export const COOKIE_EXPIRATION = 1000 * 60 * 60 * 24 * 7 * 365;
+export const COOKIE_EXPIRATION = 1000 * 60 * 60 * 24 * 7 * 365; // 7 Years
 export const COOKIE_NAME = 'auth_token';
 export const CONFIRM_USER_PREFIX = 'user-confirmation:';
 export const FORGOT_PASSWORD_PREFIX = 'forgot-password:';
-export const ORDER_EXPIRATION_TIME = 60 * 15;
+export const ORDER_EXPIRATION_TIME = 60 * 15; // 15 Minutes
