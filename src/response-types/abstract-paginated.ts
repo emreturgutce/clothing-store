72f64e3b7438505891494a903c3075d9983ae036
emreturgutce <emrebursalee@gmail.com>
2020-11-04 13:48:12 +0300

    abstract paginated response class implemented

---
+++ src/response-types/abstract-paginated.ts
@@ -0,0 +1,16 @@
+import { ClassType, Field, Int, ObjectType } from 'type-graphql';
+
+export function PaginatedResponse<TItem>(TItemClass: ClassType<TItem>) {
+  @ObjectType({ isAbstract: true })
+  abstract class PaginatedResponseClass {
+    @Field(() => [TItemClass])
+    items!: TItem[];
+
+    @Field(() => Int)
+    total!: number;
+
+    @Field()
+    hasMore!: boolean;
+  }
+  return PaginatedResponseClass;
+}
