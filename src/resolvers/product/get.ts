506ca358beb275858a9349e8e1ca8591f14c1688
emreturgutce <emrebursalee@gmail.com>
2020-11-04 13:16:30 +0300

    basic pagination for get products resolver

--- src/resolvers/product/get.ts
+++ src/resolvers/product/get.ts
@@ -1,12 +1,26 @@
-import { Authorized, Query, Resolver } from 'type-graphql';
+import { Arg, Authorized, Query, Resolver } from 'type-graphql';
 import { Product } from '../../models/product';
 
 @Resolver()
 export class GetProductsResolver {
   @Authorized()
   @Query(() => [Product], { nullable: true })
-  async getProducts(): Promise<Product[]> {
-    const products = await Product.find({ relations: ['owner', 'categories'] });
+  async getProducts(
+    @Arg('take') take: number,
+    @Arg('skip') skip: number,
+  ): Promise<Product[]> {
+    const [products, total] = await Product.findAndCount({
+      relations: ['owner', 'categories'],
+      take,
+      skip: skip * take,
+    });
+
+    if (take) {
+      console.log({
+        page: Math.ceil((total - take * skip) / take),
+        lastPage: Math.ceil(total / take),
+      });
+    }
 
     return products;
   }
