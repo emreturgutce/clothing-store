b5f527ec78f98bd97639def3d327e9c744977bac
emreturgutce <emrebursalee@gmail.com>
2020-11-04 13:48:42 +0300

    pagination input type

---
+++ src/input-types/pagination-input.ts
@@ -0,0 +1,13 @@
+import { Min } from 'class-validator';
+import { Field, InputType, Int } from 'type-graphql';
+
+@InputType()
+export class PaginationInput {
+  @Field(() => Int)
+  @Min(0, { message: 'take must be 0 or more' })
+  take!: number;
+
+  @Field(() => Int)
+  @Min(0, { message: 'skip must be 0 or more' })
+  skip!: number;
+}
