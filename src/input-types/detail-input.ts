f4482fa433276bd715142194df2ee7040e5fa46b
emreturgutce <emrebursalee@gmail.com>
2020-11-06 15:20:47 +0300

    fields made nullable

--- src/input-types/detail-input.ts
+++ src/input-types/detail-input.ts
@@ -3,11 +3,11 @@ import { Field, InputType } from 'type-graphql';
 
 @InputType()
 export class DetailInput {
-  @Field()
+  @Field({ nullable: true })
   @Length(2, 255, { message: 'name must be between 2 and 255 characters' })
   name?: string;
 
-  @Field()
+  @Field({ nullable: true })
   @IsPhoneNumber('TR', { message: 'phone number must be valid' })
   phone?: string;
 }
