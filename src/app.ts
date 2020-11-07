05d39df7538846d2a9a70c1660c87e06470435f4
emreturgutce <emrebursalee@gmail.com>
2020-11-07 12:21:25 +0300

    using session config function

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,13 @@
 import express from 'express';
-import session from 'express-session';
 import cors from 'cors';
-import connectRedis from 'connect-redis';
 import helmet from 'helmet';
-import { SESSION_SECRET, NODE_ENV, redis } from './config';
-import { COOKIE_EXPIRATION, COOKIE_NAME } from './constants';
-
-const RedisStore = connectRedis(session);
+import { createSession } from './config';
 
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 
-app.use(
-  session({
-    store: new RedisStore({
-      client: redis,
-    }),
-    name: COOKIE_NAME,
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: NODE_ENV === 'production',
-      maxAge: COOKIE_EXPIRATION,
-    },
-  }),
-);
+app.use(createSession());
 
 export { app };
