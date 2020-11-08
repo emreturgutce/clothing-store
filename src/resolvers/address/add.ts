27434a0233ab896a20dc64d37bb1a86ddaef0bb0
emreturgutce <emrebursalee@gmail.com>
2020-11-08 11:49:14 +0300

    using user roles

--- src/resolvers/address/add.ts
+++ src/resolvers/address/add.ts
@@ -2,23 +2,19 @@ import { Arg, Authorized, Ctx, Mutation, Resolver } from 'type-graphql';
 import jwt from 'jsonwebtoken';
 import { AddressInput } from '../../input-types/address-input';
 import { Address } from '../../models/address';
-import { Context } from '../../types';
+import { Context, UserRoles } from '../../types';
 import { JWT_SECRET } from '../../config';
 import { User } from '../../models/user';
 
 @Resolver()
 export class AddAddressResolver {
-  @Authorized()
+  @Authorized([UserRoles.user, UserRoles.admin])
   @Mutation(() => Address, { nullable: true })
   async addAddress(
     @Arg('data') { addressLines, city, zipcode }: AddressInput,
     @Ctx() { req }: Context,
   ): Promise<Address | null> {
-    if (!req.session) {
-      return null;
-    }
-
-    const userId = jwt.verify(req.session.userId, JWT_SECRET);
+    const userId = jwt.verify(req.session!.userId, JWT_SECRET);
 
     const user = await User.findOneOrFail({ where: { id: userId } });
 
