bbdba80c2a45391da96f8a2c4c02d59e9ecaebe7
emreturgutce <emrebursalee@gmail.com>
2020-11-08 11:50:02 +0300

    more advanced get by id with user roles

--- src/resolvers/address/get-by-id.ts
+++ src/resolvers/address/get-by-id.ts
@@ -1,30 +1,30 @@
 import { Arg, Authorized, Ctx, Query, Resolver } from 'type-graphql';
 import jwt from 'jsonwebtoken';
 import { Address } from '../../models/address';
-import { Context } from '../../types';
+import { Context, UserRoles } from '../../types';
 import { JWT_SECRET } from '../../config';
 import { User } from '../../models/user';
 
 @Resolver()
 export class GetAddressByIdResolver {
-  @Authorized()
+  @Authorized([UserRoles.user, UserRoles.admin])
   @Query(() => Address, { nullable: true })
   async getAddressById(
-    @Arg('id') id: string,
+    @Arg('addressId') addressId: string,
     @Ctx() { req }: Context,
   ): Promise<Address | null> {
-    if (!req.session) {
-      return null;
-    }
-
-    const userId = jwt.verify(req.session.userId, JWT_SECRET);
+    const userId = jwt.verify(req.session!.userId, JWT_SECRET);
 
     const user = await User.findOneOrFail({ where: { id: userId } });
 
-    const address = await Address.findOneOrFail({
-      where: { id, user },
-    });
+    if (user.role.name === UserRoles.user) {
+      return Address.getFromUser(addressId, user);
+    }
 
-    return address;
+    if (user.role.name === UserRoles.admin) {
+      return Address.getFromUser(addressId);
+    }
+
+    return null;
   }
 }
