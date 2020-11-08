a432f456c8a3cae8345f1ab1c9c5cfbc5f2850e5
emreturgutce <emrebursalee@gmail.com>
2020-11-08 11:03:09 +0300

    quick refactor

--- src/resolvers/product/get-by-user.ts
+++ src/resolvers/product/get-by-user.ts
@@ -1,4 +1,4 @@
-import { Authorized, Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Authorized, Ctx, Query, Resolver } from 'type-graphql';
 import jwt from 'jsonwebtoken';
 import { Product } from '../../models/product';
 import { Context } from '../../types';
@@ -8,13 +8,24 @@ import { JWT_SECRET } from '../../config';
 export class GetProductByUserResolver {
   @Authorized()
   @Query(() => [Product])
-  async getProductByUser(@Ctx() { req }: Context): Promise<Product[]> {
+  async getProductByUser(
+    @Ctx() { req }: Context,
+    @Arg('userId', { nullable: true }) userId?: string,
+  ): Promise<Product[]> {
+    if (userId) {
+      const products = await Product.find({
+        where: { owner: { id: userId } },
+      });
+
+      return products;
+    }
+
     const id = jwt.verify(req.session!.userId, JWT_SECRET);
 
-    const product = await Product.find({
+    const products = await Product.find({
       where: { owner: { id } },
     });
 
-    return product;
+    return products;
   }
 }
