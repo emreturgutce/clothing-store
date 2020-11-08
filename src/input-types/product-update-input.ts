a6579ade5e2e238ffa0b4d44464d994a408d5004
emreturgutce <emrebursalee@gmail.com>
2020-11-08 12:09:05 +0300

    update product input added

---
+++ src/input-types/product-update-input.ts
@@ -0,0 +1,28 @@
+import { IsNumber, Length, Min } from 'class-validator';
+import { Field, InputType } from 'type-graphql';
+
+@InputType()
+export class ProductUpdateInput {
+  @Field({ nullable: true })
+  @Length(2, 255, { message: 'name must be between 2 and 255 characters' })
+  name?: string;
+
+  @Field({ nullable: true })
+  @IsNumber(undefined, { message: 'price must be number' })
+  @Min(0)
+  price?: number;
+
+  @Field({ nullable: true })
+  @Length(2, 255, {
+    message: 'description must be between 2 and 255 characters',
+  })
+  description?: string;
+
+  @Field({ nullable: true })
+  @IsNumber(undefined, { message: 'price must be number' })
+  @Min(0)
+  stock?: number;
+
+  @Field(() => [String], { nullable: true })
+  categoryNames?: string[];
+}
