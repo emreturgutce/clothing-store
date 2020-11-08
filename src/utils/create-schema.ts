340b7e597d098ce5d68ce14fca980dbe23f1705e
emreturgutce <emrebursalee@gmail.com>
2020-11-08 17:05:05 +0300

    production changes

--- src/utils/create-schema.ts
+++ src/utils/create-schema.ts
@@ -2,10 +2,16 @@ import { buildSchema } from 'type-graphql';
 import path from 'path';
 import { authChecker } from './auth-checker';
 import { AvatarScalar } from '../types/avatar-scalar';
+import { NODE_ENV } from '../config';
 
 export const createSchema = async () => {
   return buildSchema({
-    resolvers: [path.join(__dirname, '../resolvers/*/*.ts')],
+    resolvers: [
+      path.join(
+        __dirname,
+        `../resolvers/*/*.${NODE_ENV === 'production' ? 'js' : 'ts'}`,
+      ),
+    ],
     authChecker,
     scalarsMap: [
       {
