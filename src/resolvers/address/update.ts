9a8c28beb817744b011b223664533a7f54b9d37f
emreturgutce <emrebursalee@gmail.com>
2020-11-08 11:51:08 +0300

    more advanced update address with user roles

--- src/resolvers/address/update.ts
+++ src/resolvers/address/update.ts
@@ -1,40 +1,24 @@
 import { Arg, Authorized, Ctx, Mutation, Resolver } from 'type-graphql';
 import jwt from 'jsonwebtoken';
 import { Address } from '../../models/address';
-import { Context } from '../../types';
+import { Context, UserRoles } from '../../types';
 import { JWT_SECRET } from '../../config';
 import { User } from '../../models/user';
 import { OptionalAddressInput } from '../../input-types/address-input';
 
 @Resolver()
 export class UpdateAddressResolver {
-  @Authorized()
+  @Authorized([UserRoles.user, UserRoles.admin])
   @Mutation(() => Boolean)
   async updateAddress(
-    @Arg('id') id: string,
+    @Arg('addressId') addressId: string,
     @Arg('data') optionalAddressInput: OptionalAddressInput,
     @Ctx() { req }: Context,
   ): Promise<boolean> {
-    if (!req.session) {
-      return false;
-    }
-
-    const userId = jwt.verify(req.session.userId, JWT_SECRET);
+    const userId = jwt.verify(req.session!.userId, JWT_SECRET);
 
     const user = await User.findOneOrFail({ where: { id: userId } });
 
-    const result = await Address.update(
-      {
-        id,
-        user,
-      },
-      optionalAddressInput,
-    );
-
-    if (!result.affected) {
-      return false;
-    }
-
-    return true;
+    return Address.updateFromUser(addressId, optionalAddressInput, user);
   }
 }
