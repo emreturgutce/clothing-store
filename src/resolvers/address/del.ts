0a058e137a01dda098a0f8ae9464ff66a4c87b09
emreturgutce <emrebursalee@gmail.com>
2020-11-08 11:49:40 +0300

    more advanced deletion with user roles

--- src/resolvers/address/del.ts
+++ src/resolvers/address/del.ts
@@ -1,35 +1,30 @@
 import { Arg, Authorized, Ctx, Mutation, Resolver } from 'type-graphql';
 import jwt from 'jsonwebtoken';
 import { Address } from '../../models/address';
-import { Context } from '../../types';
+import { Context, UserRoles } from '../../types';
 import { JWT_SECRET } from '../../config';
 import { User } from '../../models/user';
 
 @Resolver()
-export class DelAddressResolver {
-  @Authorized()
+export class DeleteAddressResolver {
+  @Authorized([UserRoles.user, UserRoles.admin])
   @Mutation(() => Boolean)
-  async delAddress(
-    @Arg('id') id: string,
+  async deleteAddress(
+    @Arg('addressId') addressId: string,
     @Ctx() { req }: Context,
   ): Promise<boolean> {
-    if (!req.session) {
-      return false;
+    const userId = jwt.verify(req.session!.userId, JWT_SECRET);
+
+    const user = await User.findOneOrFail({ where: { id: userId } });
+
+    if (user.role.name === UserRoles.user) {
+      return Address.deleteFromUser(addressId, user);
     }
 
-    const userId = jwt.verify(req.session.userId, JWT_SECRET);
-
-    const user = await User.findOne({ where: { id: userId } });
-
-    const result = await Address.delete({
-      id,
-      user,
-    });
-
-    if (!result.affected) {
-      return false;
+    if (user.role.name === UserRoles.admin) {
+      return Address.deleteFromUser(addressId);
     }
 
-    return true;
+    return false;
   }
 }
