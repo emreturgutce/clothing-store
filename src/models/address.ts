08499fff0f387b8f1602c2ba2a3e992b38ae2c34
emreturgutce <emrebursalee@gmail.com>
2020-11-08 11:48:05 +0300

    deleteFromUser, getFromUser, and updateFromUser methods added

--- src/models/address.ts
+++ src/models/address.ts
@@ -2,6 +2,7 @@ import { Column, Entity, ManyToOne } from 'typeorm';
 import { Field, ObjectType } from 'type-graphql';
 import { User } from './user';
 import { ExternalEntity } from './base-entity';
+import { OptionalAddressInput } from '../input-types/address-input';
 
 @ObjectType()
 @Entity()
@@ -21,4 +22,38 @@ export class Address extends ExternalEntity {
   @Field(() => User)
   @ManyToOne(() => User, { onDelete: 'CASCADE', eager: true })
   user!: User;
+
+  static async deleteFromUser(
+    addressId: string,
+    user?: User,
+  ): Promise<boolean> {
+    return !!(
+      await this.delete({
+        id: addressId,
+        user: user || undefined,
+      })
+    ).affected;
+  }
+
+  static async getFromUser(addressId: string, user?: User): Promise<Address> {
+    return this.findOneOrFail({
+      where: { id: addressId, user: user || undefined },
+    });
+  }
+
+  static async updateFromUser(
+    addressId: string,
+    optionalAddressInput: OptionalAddressInput,
+    user?: User,
+  ): Promise<boolean> {
+    return !!(
+      await Address.update(
+        {
+          id: addressId,
+          user,
+        },
+        optionalAddressInput,
+      )
+    ).affected;
+  }
 }
