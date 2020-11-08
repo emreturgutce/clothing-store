f4d8d5f8980750f5c65da80114e1c823d0df4a93
emreturgutce <emrebursalee@gmail.com>
2020-11-08 10:05:48 +0300

    advanced delete product

--- src/resolvers/product/del.ts
+++ src/resolvers/product/del.ts
@@ -1,17 +1,50 @@
-import { Arg, Authorized, Mutation, Resolver } from 'type-graphql';
+import { Arg, Authorized, Ctx, Mutation, Resolver } from 'type-graphql';
+import jwt from 'jsonwebtoken';
 import { Product } from '../../models/product';
+import { Context, UserRoles } from '../../types';
+import { JWT_SECRET } from '../../config';
+import { User } from '../../models/user';
 
 @Resolver()
-export class DelProductResolver {
-  @Authorized()
+export class DeleteProductResolver {
+  @Authorized([UserRoles.user, UserRoles.admin])
   @Mutation(() => Boolean)
-  async delProduct(@Arg('id') id: string): Promise<boolean> {
-    const result = await Product.delete(id);
+  async deleteProduct(
+    @Arg('productId') productId: string,
+    @Ctx() { req }: Context,
+  ): Promise<boolean> {
+    const userId = jwt.verify(req.session!.userId, JWT_SECRET);
 
-    if (!result.affected) {
-      return false;
+    const user = await User.findOneOrFail({
+      where: { id: userId },
+      relations: ['products'],
+    });
+
+    if (user.role.name === UserRoles.user) {
+      const val = user.checkIfUserIsOwner(productId);
+
+      if (!val) {
+        return false;
+      }
+
+      const result = await Product.delete(productId);
+
+      if (!result.affected) {
+        return false;
+      }
+
+      return true;
+    }
+    if (user.role.name === UserRoles.admin) {
+      const result = await Product.delete(productId);
+
+      if (!result.affected) {
+        return false;
+      }
+
+      return true;
     }
 
-    return true;
+    return false;
   }
 }
