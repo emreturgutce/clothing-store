87c0293a15e220dc2ccfd901c643d78a895e6b7f
emreturgutce <emrebursalee@gmail.com>
2020-11-09 13:04:35 +0300

    using user roles

--- src/resolvers/order/cancel-order.ts
+++ src/resolvers/order/cancel-order.ts
@@ -1,13 +1,13 @@
 import { Arg, Authorized, Ctx, Mutation, Resolver } from 'type-graphql';
 import jwt from 'jsonwebtoken';
 import { Order } from '../../models/order';
-import { Context, OrderStatus } from '../../types';
+import { Context, OrderStatus, UserRoles } from '../../types';
 import { JWT_SECRET } from '../../config';
 import { CancelOrderInput } from '../../input-types/cancel-order-input';
 
 @Resolver()
 export class CancelOrderResolver {
-  @Authorized()
+  @Authorized([UserRoles.user, UserRoles.admin])
   @Mutation(() => Order, { nullable: true })
   async cancelOrder(
     @Arg('data') { id }: CancelOrderInput,
