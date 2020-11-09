fb2d8def21db51284fcb42aad2923130091c01d1
emreturgutce <emrebursalee@gmail.com>
2020-11-09 13:20:09 +0300

    redundant mw deleted

--- src/resolvers/user/me.ts
+++ src/resolvers/user/me.ts
@@ -3,13 +3,11 @@ import jwt from 'jsonwebtoken';
 import { User } from '../../models/user';
 import { Context } from '../../types';
 import { JWT_SECRET } from '../../config';
-import { CacheControl } from '../../utils/cache-control';
 
 @Resolver()
 export class MeResolver {
   @Authorized()
   @Query(() => User, { nullable: true })
-  @CacheControl({ maxAge: 15 })
   async me(@Ctx() { req }: Context): Promise<User | null> {
     const id = jwt.verify(req.session!.userId, JWT_SECRET);
 
