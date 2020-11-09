0b3d0249faed518f4d2a94a557449db6e22b3240
emreturgutce <emrebursalee@gmail.com>
2020-11-09 12:40:22 +0300

    a bit refactor and using user roles

--- src/resolvers/category/get-by-id.ts
+++ src/resolvers/category/get-by-id.ts
@@ -1,13 +1,12 @@
 import { Arg, Authorized, Query, Resolver } from 'type-graphql';
 import { Category } from '../../models/category';
+import { UserRoles } from '../../types';
 
 @Resolver()
 export class GetCategoryByIdResolver {
-  @Authorized()
+  @Authorized([UserRoles.user, UserRoles.admin])
   @Query(() => Category, { nullable: true })
   async getCategoryById(@Arg('id') id: string): Promise<Category | undefined> {
-    const category = await Category.findOne({ where: { id } });
-
-    return category;
+    return Category.findOne({ where: { id } });
   }
 }
