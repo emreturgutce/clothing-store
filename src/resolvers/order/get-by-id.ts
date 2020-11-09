fb3219a53ca8f0a76d9950c973bd421c45e8cb48
emreturgutce <emrebursalee@gmail.com>
2020-11-09 13:05:49 +0300

    using user roles and a bit refactor

--- src/resolvers/order/get-by-id.ts
+++ src/resolvers/order/get-by-id.ts
@@ -1,11 +1,30 @@
-import { Arg, Authorized, Query, Resolver } from 'type-graphql';
+import { Arg, Authorized, Ctx, Query, Resolver } from 'type-graphql';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../../config';
 import { Order } from '../../models/order';
+import { Context, UserRoles } from '../../types';
+import { User } from '../../models/user';
 
 @Resolver()
 export class GetOrderByIdResolver {
-  @Authorized()
+  @Authorized([UserRoles.user, UserRoles.admin])
   @Query(() => Order, { nullable: true })
-  async getOrderById(@Arg('id') id: string): Promise<Order | undefined> {
-    return Order.findOneOrFail(id);
+  async getOrderById(
+    @Arg('id') id: string,
+    @Ctx() { req }: Context,
+  ): Promise<Order> {
+    const userId = jwt.verify(req.session!.userId, JWT_SECRET);
+
+    const user = await User.findOneOrFail({ where: { id: userId } });
+
+    if (user.role.name === UserRoles.admin) {
+      const order = await Order.findOneOrFail({ where: { id } });
+
+      return order;
+    }
+
+    const order = await Order.findOneOrFail({ where: { id, user } });
+
+    return order;
   }
 }
