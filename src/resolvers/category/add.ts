caa96508796e58e2bcf374a961624e655f894304
emreturgutce <emrebursalee@gmail.com>
2020-11-09 12:39:15 +0300

    using user roles and a bit refactoring

--- src/resolvers/category/add.ts
+++ src/resolvers/category/add.ts
@@ -1,19 +1,17 @@
 import { Arg, Authorized, Mutation, Resolver } from 'type-graphql';
 import { Category } from '../../models/category';
 import { CategoryInput } from '../../input-types/category-input';
+import { UserRoles } from '../../types';
 
 @Resolver()
 export class AddCategoryResolver {
-  @Authorized()
+  @Authorized([UserRoles.user, UserRoles.admin])
   @Mutation(() => Category, { nullable: true })
   async addCategory(
-    @Arg('data')
-    { name }: CategoryInput,
+    @Arg('data') { name }: CategoryInput,
   ): Promise<Category | null> {
-    const category = await Category.create({
+    return Category.create({
       name,
     }).save();
-
-    return category;
   }
 }
