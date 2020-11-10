c47f7cb6346ace099bec149a462ac9f5403e4570
emreturgutce <emrebursalee@gmail.com>
2020-11-10 18:58:56 +0300

    basic register test

--- src/resolvers/user/__tests__/register.test.ts
+++ src/resolvers/user/__tests__/register.test.ts
@@ -1,17 +1,47 @@
-import { gql } from 'apollo-server-core';
+import { ApolloServerBase } from 'apollo-server-core';
+import { createTestClient } from 'apollo-server-testing';
+import { initializeApolloServerBase } from '../../../config/apollo-server-base';
+import { helloQuery, registerMutation } from '../../../mutations';
 
-const helloQuery = gql`
-  {
-    hello
-  }
-`;
+let server: ApolloServerBase;
+(async () => {
+  server = await initializeApolloServerBase();
+})();
 
 describe('Register Mutation Test Suite', () => {
   it('Hello Query', async () => {
-    const res = await global.graphqlClient.query({ query: helloQuery });
+    const { query } = createTestClient(server);
+
+    type Data = { hello: string };
+
+    const res = await query<Data>({ query: helloQuery });
+
+    expect(res).toBeDefined();
+    expect(res.data?.hello).toEqual('hello World');
+    expect(res.errors).toBeUndefined();
+    expect(res.extensions).toBeUndefined();
+  });
+
+  it('Register Mutation', async () => {
+    const { mutate } = createTestClient(server);
+
+    const res = await mutate({
+      mutation: registerMutation,
+      variables: {
+        data: {
+          name: 'emre',
+          phone: '+905350149645',
+          email: 'emreturgut@mail.com',
+          password: '123456',
+        },
+      },
+    });
 
     console.log(res);
 
     expect(res).toBeDefined();
+    // expect(res.data?.hello).toEqual('hello World');
+    // expect(res.errors).toBeUndefined();
+    // expect(res.extensions).toBeUndefined();
   });
 });
