c720131bdb4c72ed80841baaacf7fc0e69045db2
emreturgutce <emrebursalee@gmail.com>
2020-11-10 16:52:01 +0300

    quick fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,5 @@ export * from './clear-database';
 export * from './create-confirmation-url';
 export * from './create-schema';
 export * from './format-error';
-export * from './g-call';
 export * from './send-email';
 export * from './create-fake-user';
