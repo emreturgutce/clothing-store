4a9007c7eed51f5013fe7dc71a362cd9c688a8f6
emreturgutce <emrebursalee@gmail.com>
2020-11-10 11:52:46 +0300

    using s3 to store images

--- src/resolvers/user/avatar.ts
+++ src/resolvers/user/avatar.ts
@@ -1,10 +1,12 @@
 import { Arg, Authorized, Ctx, Mutation, Resolver } from 'type-graphql';
 import { FileUpload, GraphQLUpload } from 'graphql-upload';
 import jwt from 'jsonwebtoken';
-import streamToPromise from 'stream-to-promise';
+import { v4 as uuid } from 'uuid';
 import { User } from '../../models/user';
 import { Context } from '../../types';
 import { JWT_SECRET } from '../../config';
+import { uploadAvatarToS3 } from '../../utils/upload-avatar-to-s3';
+import { deleteAvatarFromS3 } from '../../utils/delete-avatar-from-s3';
 
 @Resolver()
 export class AvatarResolver {
@@ -18,21 +20,29 @@ export class AvatarResolver {
     return new Promise(async (resolve, reject) => {
       const id = jwt.verify(req.session!.userId, JWT_SECRET);
 
-      const user = await User.findOneOrFail({
-        where: { id },
-      });
+      const user = await User.findOneOrFail({ where: { id } });
 
       const readStream = file.createReadStream();
 
-      streamToPromise(readStream)
-        .then(async (data) => {
-          user.avatar = data;
-          await user.save();
-          resolve(true);
-        })
-        .catch((err) => {
-          reject(err);
-        });
+      const extension = file.filename.split('.')[1];
+
+      const avatarId = `${uuid()}.${extension}`;
+
+      try {
+        if (user.avatarId) {
+          await deleteAvatarFromS3(user.avatarId);
+        }
+
+        await uploadAvatarToS3(avatarId, readStream);
+
+        user.avatarId = avatarId;
+
+        await user.save();
+
+        resolve(true);
+      } catch (err) {
+        reject(err);
+      }
     });
   }
 }
