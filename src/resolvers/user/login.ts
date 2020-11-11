b32089a9648cbba621aad8443e13b359eeaab1cf
emreturgutce <emrebursalee@gmail.com>
2020-11-11 13:12:47 +0300

    throwing authentication error in fail cases

--- src/resolvers/user/login.ts
+++ src/resolvers/user/login.ts
@@ -1,4 +1,5 @@
 import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
+import { AuthenticationError } from 'apollo-server-express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { LoginInput } from '../../input-types/login-input';
@@ -13,12 +14,16 @@ export class LoginUserResolver {
     @Arg('data') { email, password }: LoginInput,
     @Ctx() ctx: Context,
   ): Promise<User | null> {
-    const user = await User.findOneOrFail({ where: { email } });
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new AuthenticationError('Wrong email or password');
+    }
 
     const isValid = await bcrypt.compare(password, user.password);
 
     if (!isValid) {
-      return null;
+      throw new AuthenticationError('Wrong email or password');
     }
 
     ctx.req.session!.userId = jwt.sign(user.id, JWT_SECRET);
