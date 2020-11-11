b395c923c5a4194417ecfc8990691970cedb4a7c
emreturgutce <emrebursalee@gmail.com>
2020-11-11 10:33:13 +0300

    getting more fields back

--- src/mutations/register.ts
+++ src/mutations/register.ts
@@ -5,6 +5,23 @@ export const registerMutation = gql`
     register(data: $data) {
       id
       email
+      confirmed
+      avatarId
+      role {
+        id
+        name
+        createdAt
+        updatedAt
+      }
+      detail {
+        id
+        name
+        phone
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
     }
   }
 `;
