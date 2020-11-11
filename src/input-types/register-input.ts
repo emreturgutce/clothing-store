9a065d5991a662f73a9d8bb973b6e72b307d8172
emreturgutce <emrebursalee@gmail.com>
2020-11-11 10:18:11 +0300

    fields made optional

--- src/input-types/register-input.ts
+++ src/input-types/register-input.ts
@@ -1,16 +1,18 @@
-import { IsPhoneNumber, Length } from 'class-validator';
+import { IsOptional, IsPhoneNumber, Length } from 'class-validator';
 import { Field, InputType } from 'type-graphql';
 import { LoginInput } from './login-input';
 
 @InputType()
 export class RegisterInput extends LoginInput {
   @Field({ nullable: true })
+  @IsOptional()
   @Length(2, 255, {
     message: 'name must be between 2 and 255 characters',
   })
   name?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
   @IsPhoneNumber('TR', { message: 'phone number must be valid' })
   phone?: string;
 }
