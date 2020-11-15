0912369b592c94dfdbefb7d562de13c60880df97
emreturgutce <emrebursalee@gmail.com>
2020-11-15 17:47:50 +0300

    registering enum

--- src/types/order-status.ts
+++ src/types/order-status.ts
@@ -1,6 +1,12 @@
+import { registerEnumType } from 'type-graphql';
+
 export enum OrderStatus {
   awaitingPayment = 'AWAITING_PAYMENT',
   cancelled = 'CANCELLED',
   completed = 'COMPLETED',
   expired = 'EXPIRED',
 }
+
+registerEnumType(OrderStatus, {
+  name: 'OrderStatus',
+});
