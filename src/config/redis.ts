559a179bca5e017aeba61f5d05ec082844a8c37a
emreturgutce <emrebursalee@gmail.com>
2020-11-16 12:52:36 +0300

    using REDIS_URL if REDIS_HOST undefined

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,7 +1,7 @@
 import Redis from 'ioredis';
-import { REDIS_HOST, REDIS_PORT } from '.';
+import { REDIS_HOST, REDIS_PORT, REDIS_URL } from '.';
 
 export const redis = new Redis({
-  host: REDIS_HOST,
+  host: REDIS_HOST || REDIS_URL,
   port: parseInt(REDIS_PORT, 10),
 });
