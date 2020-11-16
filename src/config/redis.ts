6ebe7d264d1206b87618eafc289eb0078fbbef58
emreturgutce <emrebursalee@gmail.com>
2020-11-16 14:47:39 +0300

    redis config error handling

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,19 @@
-import Redis from 'ioredis';
+import Redis, { Redis as RedisType } from 'ioredis';
 import { REDIS_HOST, REDIS_PORT, REDIS_USER, REDIS_PASSWORD } from '.';
 
-export const redis = new Redis({
-  username: REDIS_USER,
-  host: REDIS_HOST,
-  password: REDIS_PASSWORD,
-  port: parseInt(REDIS_PORT, 10),
-});
+let redis: RedisType | undefined;
+
+try {
+  redis = new Redis({
+    username: REDIS_USER,
+    host: REDIS_HOST,
+    password: REDIS_PASSWORD,
+    port: parseInt(REDIS_PORT, 10),
+  });
+
+  console.log(`👹  Connected to Redis`.bgMagenta);
+} catch (err) {
+  console.error(`Error occurred connecting Redis:\n${err}`.red.bold);
+}
+
+export { redis };
